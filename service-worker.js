2bbebfe9fbf78c5fb096dc9b656e06c2d39e7e98
sergiof2u <adharajoyas@gmail.com>
2025-05-28 21:51:39 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,47 @@
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('mi-semana-cache').then(function(cache) {
-      return cache.addAll([
-        'index.html',
-        'manifest.json',
-        'icono.png'
-      ]);
+const CACHE_NAME = 'mi-semana-v2';
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  'icono.png'
+];
+
+// Instalación: cachear archivos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+// Activación: limpiar caches viejos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Interceptar fetch y usar cache o red
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
+});
+
+// Notificar a la app cuando hay una nueva versión
+self.addEventListener('message', (event) => {
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
